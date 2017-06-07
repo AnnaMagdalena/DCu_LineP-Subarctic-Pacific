ee5f225aca6097bf03e83934907cf17e8a9893fd
Anna <ania.posacka@gmail.com>
2017-06-06 17:34:05 -0700

    Tidy up code for Fig.5

--- Fig5_Cu-studies-in-NPacific-profiles/Fig5_dCu-studies-NPacific.R
+++ Fig5_Cu-studies-in-NPacific-profiles/Fig5_dCu-studies-NPacific.R
@@ -5,7 +5,7 @@ library(cowplot)
 library(tidyverse)
 
 # load the dataset
-data <-read_csv('C:/Users/Ania/DCu_LineP-Subarctic-Pacific/Fig5_Cu-studies-in-NPacific-profiles/Cu_NPacific-Fig5-MS.csv')
+data <-read_csv('C~/Cu_NPacific-Fig5-MS.csv')
 
 # set station and references as factors
 data$Station <- as.factor(data$Station)
@@ -20,11 +20,13 @@ p1 <- data %>%
 	ggplot(aes(x=dCu, y=Depth, group=Station)) +
 	geom_path(size=0.3)+
 	geom_point(aes(shape=Station,color=Station), size=2)+
-	scale_color_manual(name="Region 1",labels = c("5, Tanita et al., 2015","6, Moffett & Dupont, 2007",
-																								"CR27, Takano et al., 2014","TR15, Takano et al., 2014"),
+	scale_color_manual(name="Region 1",
+										 labels = c("5, Tanita et al., 2015","6, Moffett & Dupont, 2007",
+								     "CR27, Takano et al., 2014","TR15, Takano et al., 2014"),
 										 values=c("#000000","#000000","#000000","#999999"))+
-	scale_shape_manual(name="Region 1",labels = c("5, Tanita et al., 2015","6, Moffett & Dupont, 2007",
-																								"CR27, Takano et al., 2014","TR15, Takano et al., 2014"),
+	scale_shape_manual(name="Region 1",
+										 labels = c("5, Tanita et al., 2015","6, Moffett & Dupont, 2007",
+										"CR27, Takano et al., 2014","TR15, Takano et al., 2014"),
 										 values=c(1,2,17,17))+
 	scale_y_reverse(expand=c(0.01,0),limits=c(6700,0), breaks=seq(0,6700,by=1000))+
 	scale_x_continuous(position="top",limits=c(0.8,4.9))+
@@ -32,17 +34,17 @@ p1 <- data %>%
 	guides(shape=guide_legend(override.aes = list(size=2)))+
 	ylab("Depth (m)")+
 	theme_bw()+
-	theme(#legend.key.size = unit(0.25, "cm"),
+	theme(legend.key.size = unit(0.25, "cm"),
 				legend.position="none",
-				#axis.title.y = element_blank(),
-				#axis.text.y = element_blank(),
-		    #legend.title=element_text(size=8),
-				#legend.text = element_text(size=8),
+				axis.title.y = element_blank(),
+				axis.text.y = element_blank(),
+		    legend.title=element_text(size=8),
+				legend.text = element_text(size=8),
 				axis.title = element_text(size=10),
 				panel.grid.major = element_blank(),
 				panel.grid.minor = element_blank(),
-				axis.text = element_text(size=9.5))
-			#	legend.position = c(.50,.13))
+				axis.text = element_text(size=9.5),
+			  legend.position = c(.50,.13))
 
 #~~~~~~~~~~~~ plotting region 2~~~~~~~~~~~~~~~~~~
 
@@ -51,8 +53,9 @@ p2 <- data %>%
 	ggplot(aes(x=dCu, y=Depth, group=Station)) +
 	geom_path(size=0.3)+
 	geom_point(aes(shape=Station), size=2)+
-	scale_shape_manual(name="Region 2",labels = 
-										 	c("19, Fujishima et al., 2001","4, Moffett & Dupont, 2007"),values=c(16,2))+
+	scale_shape_manual(name="Region 2",
+										 labels = c("19, Fujishima et al., 2001","4, Moffett & Dupont, 2007"),
+										 values=c(16,2))+
 	scale_y_reverse(expand=c(0.01,0),limits=c(6500,0), breaks=seq(0,6500,by=1000))+
 	scale_x_continuous(position="top",limits=c(0.8,4.9))+
 	xlab (expression("dCu"~(nmol~kg^{-1})))+
@@ -181,7 +184,7 @@ p7 <- data %>%
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~ region 7~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-p6 <- data %>%
+p7 <- data %>%
 	filter(Station %in% c("226","TR7"))%>%
 	ggplot(aes(x=dCu, y=Depth, group=Station)) +
 	geom_path(size=0.4)+
@@ -192,25 +195,23 @@ p6 <- data %>%
 	scale_y_reverse(expand=c(0.01,0),limits=c(8000,0), breaks=seq(0,8000,by=1000))+
 	scale_x_continuous(position="top",limits=c(0.3,6.6))+
 	xlab (expression("dCu"~(nmol~kg^{-1})))+
-#	guides(shape=guide_legend(override.aes = list(size=2)))+
+	guides(shape=guide_legend(override.aes = list(size=2)))+
 	ylab("Depth (m)")+
 	theme_bw()+
-	theme(#legend.key.size = unit(0.25, "cm"),
-		    #legend.title=element_text(size=8),
+	theme(legend.key.size = unit(0.25, "cm"),
+		    legend.title=element_text(size=8),
 				legend.position="none",
-	#			legend.text = element_text(size=8),
-	#			axis.text.y = element_blank(),
-	#			axis.title.y = element_blank(),
+	  		legend.text = element_text(size=8),
+				axis.text.y = element_blank(),
+				axis.title.y = element_blank(),
 				axis.title = element_text(size=10),
 				panel.grid.major = element_blank(),
 				panel.grid.minor = element_blank(),
-				axis.text = element_text(size=9.5))
-		#		legend.position = c(.43,.16))
+				axis.text = element_text(size=9.5),
+				legend.position = c(.43,.16))
 
 # Orientating the plots + saving the plots
+plots <- plot_grid (p1,p2,p3,p4,p5,p7,p6, ncol = 5,align = "h")
 
-plots <- plot_grid (p1,p2,p3,p4,p5,p6,p7, ncol = 5,align = "h")
-
-save_plot("Fig5_MS.tiff",plots, ncol = 5, nrow = 2,base_height = 3.3, base_width = 1.9, dpi=1000)
-save_plot("Fig5_MS-labels.pdf",plots, ncol = 5, nrow = 2,base_height = 3.3, base_width = 1.9, dpi=1000)
-#save_plot("Fig5_MS.eps",plots, ncol = 5, nrow = 2,base_height = 4.5, base_width = 3.5, dpi=1000)
+#save plot in the format of choice
+#save_plot("Fig5_MS-labels.pdf",plots, ncol = 5, nrow = 2,base_height = 3.3, base_width = 1.9, dpi=1000)
